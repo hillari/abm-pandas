e7885f849d019e631be50776a571900ee01bd689
hillari <hillari@gmail.com>
2020-09-14 22:32:01 -0800

    Restructure clean_data code and param file parsing

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -25,25 +25,27 @@ def get_args():
     # FIXME don't need plot stuff. Could do constant here
     if args.type == 'habitat':
         dict_index = 8
-        constant = 4
+        constant_index = 4
         plot_type = 'habitat_suitability'
+        constant = 'host_density'
         # x_label = "Habitat Suitability "
         # plot_title = "Aggregated Habitat Suitability"
         # plot_legend = "Host Density: "
     else:
         dict_index = 4
+        constant_index = 8
         plot_type = 'host_density'
-        constant = 8
+        constant = 'habitat_suitability'
         # x_label = "Host Density "
         # plot_title = "Aggregated Host Density"
         # plot_legend = "Habitat Suitability: "
-    return dict_index, plot_type, constant
+    return dict_index, constant_index, plot_type, constant
 
 
-def get_params(paramfile):
+def get_params(paramfile, dict_index, constant_index):
     # get paramfile and make dict
     # Create a dictionary that maps the run# to host_density { run#: host_density }
-    dict_index, _, constant = get_args()
+    # dict_index, _, _, constant = get_args()
     # paramfile = "/media/hill/DATA-LINUX/abm-data/host-density/testparams1hab7"
     paramfile = paramfile
     param_dict = {}
@@ -56,13 +58,13 @@ def get_params(paramfile):
 
             # attempt to add the density value to run, if it doesn't exist, create it
             try:
-                param_dict[int(result[0])].append((float(result[4])))
+                param_dict[int(result[0])].append(float(result[dict_index]), float(result[constant_index]))
             except KeyError:
-                param_dict[int(result[0])] = (float(result[4]))  # { run#: host_density }
+                param_dict[int(result[0])] = (float(result[dict_index]), float(result[constant_index]))  # { run#: host_density/habitat_suitability }
 
-    # for k, v in paramd.items():
+    # for k, v in param_dict.items():
     #     print(k, v)
-    return param_dict, constant
+    return param_dict
 
 
 def get_datafile(csvfile):
@@ -82,7 +84,7 @@ def clean_data(data):
     # filter bad lines
     # drop if necessary (prob not)
     # returns df
-    df = data # ? Because if we have arg = df and return df...?
+    df = data  # ? Because if we have arg = df and return df...?
     print("Filtering database...")
     before = datetime.now()
     print("Max before filtering...", df['tick'].max())
@@ -94,13 +96,11 @@ def clean_data(data):
     return df
 
 
-def build_df(data, paramfile):
+def build_df(data, paramfile, constant, dict_index, constant_index):
     # aggregate, parse paramfile and map, make final agg df w/std
     # returns df
-    # TODO ? make this call get_params instead of passing it? since we need so much from args anyway
 
-    param_dict, constant = get_params(paramfile)
-
+    param_dict = get_params(paramfile, dict_index, constant_index)
     print("Building df...")
     before = datetime.now()
     n_ticks_df = data.groupby(['run'], as_index=False)
@@ -108,16 +108,19 @@ def build_df(data, paramfile):
 
     # get the parameter mappings and add to df
     for key, value in param_dict.items():
-        n_ticks_df.loc[n_ticks_df['run'] == key, 'host_density'] = float(value)
-        # n_ticks_df[]
+        n_ticks_df.loc[n_ticks_df['run'] == key, 'host_density'] = float(value[0])
 
-    print("n_ticks_db ")
-    print(n_ticks_df.head())
+    # print("n_ticks_db ")
+    # print(n_ticks_df.head())
 
     final_df = n_ticks_df.groupby('host_density')['total_ixodes'].agg({'mean', 'std'})
+    final_df[constant] = param_dict[1][1]
 
     after = datetime.now()
     print("Build df runtime: ", after - before)
+
+    print("final_db ")
+    print(final_df.head())
 
     return final_df
 
@@ -127,21 +130,25 @@ def write_df(final_df):
     # no return val
     if args.first:
         writemode = 'w'
+        header = True
     else:
         writemode = 'a'
+        header = False
 
     # TODO get current dir, chagne filename
-    final_df.to_csv('/media/hill/DATA-LINUX/abm-data/host-density-olderruns/new-model/aggregate-runs/adddf', mode=writemode)
+    final_df.to_csv('/media/hill/DATA-LINUX/abm-data/host-density-olderruns/new-model/aggregate-runs/adddf', mode=writemode, header=header)
 
 
 def main():
     # Specify hard-coded files for testing here
-    paramfile = "/media/hill/DATA-LINUX/abm-data/host-density-olderruns/new-model/aggregate-runs/params_05habitat"
-    csvfile = "/media/hill/DATA-LINUX/abm-data/host-density-olderruns/new-model/aggregate-runs/density-new.2020.Jul.11_hab05"
+    paramfile = "/media/hill/DATA-LINUX/abm-data/host-density-olderruns/new-model/aggregate-runs/params_09habitat"
+    csvfile = "/media/hill/DATA-LINUX/abm-data/host-density-olderruns/new-model/aggregate-runs/density-new.2020.Jul.11hab_09"
+
+    dict_index, constant_index, plot_type, constant_str = get_args()
 
     df = get_datafile(csvfile)
     clean_df = clean_data(df)
-    final_df = build_df(clean_df, paramfile)
+    final_df = build_df(clean_df, paramfile, constant_str, dict_index, constant_index)
     write_df(final_df)
 
 
